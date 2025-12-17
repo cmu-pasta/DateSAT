9cc9eae3f1c660727d6a0cd16d99a139568bfef2
Shrey Tiwari <shreymt@gmail.com>
2025-12-17 08:30:03 -0500

    temp

--- dataset/test.py
+++ dataset/test.py
@@ -36,39 +36,34 @@ from datesmt.core import Date, Period
 # Paste a single JSON constraint object here (one from *_constraints*.jsonl)
 CONSTRAINT_JSON = r"""
 {
-    "id": "grammar-10",
+    "id": "grammar-sat-1",
     "declarations": [
       "B0: bool",
-      "B1: bool",
+      "B2: bool",
+      "B5: bool",
       "B6: bool",
-      "B8: bool",
       "D0: date",
-      "D1: date",
       "D2: date",
       "D3: date",
-      "D4: date",
       "D5: date",
+      "D6: date",
+      "D7: date",
       "D8: date",
       "D9: date",
-      "I1: int",
-      "I2: int",
       "I4: int",
-      "I5: int",
       "I6: int",
-      "I8: int"
+      "I7: int"
     ],
     "constraints": [
-      "(B6 != True) -> ((D9 + Period(3, 3, 9)) < Date(2064, 3, 18)) || D0 >= Date(1912, 4, 11) || I2 >= 2021",
-      "(B0 != True) -> (D5 <= (D2 - Period(9, 6, 4)))",
-      "I6 != 8 || D2 != (Date(2087, 12, 18) + ((Period(9, 1, 2) * 2) * 9)) || D0 <= (D2 - Period(5, 3, 0)) || D8 < (((D1 - (Period(9, 5, 0) - Period(0, 6, 5))) - (Period(0, 3, 2) - (Period(9, 2, 1) + (Period(0, 5, 6) + ((Period(9, 7, 4) + Period(0, 4, 3)) * 5))))) + ((((Period(4, 8, 9) + Period(5, 9, 4)) * 2) * 1) * 5)) || D5.year < 6 || D9.month <= (D1.year + (I1 * 2033)) || (B1 == False) -> (D4 == Date(2064, 10, 3)) || (B6 == True) -> (D9 > (Date(1930, 9, 27) + Period(8, 9, 8)))",
-      "D8 < Date(1919, 4, 5)",
-      "I4 == 23",
-      "I1 <= (I8 - 1) || D4.year < I5",
-      "D3.month != (I1 - 2038)",
-      "(B0 == True) -> (D5 == D0) || (B8 != True) -> (D2 == Date(2059, 10, 23))",
-      "D3 != (Date(2069, 6, 8) + (Period(8, 3, 2) * 8))"
+      "I4 < 2165 || (B0 != True) -> (D5 >= Date(2074, 11, 10)) || (B0 != False) -> ((((D7 + (Period(0, 0, 6) + ((Period(1, 6, 4) - Period(1, 2, 6)) * 5))) - (Period(5, 4, 8) + (Period(4, 0, 3) * 4))) + (Period(1, 4, 8) * 4)) >= (Date(1968, 4, 2) + (Period(7, 0, 5) - ((Period(8, 1, 6) - Period(6, 2, 0)) * 4)))) || D5 == D7",
+      "D8 != D8 || D9 < Date(1935, 11, 7)",
+      "(B5 == False) -> (D0 >= D9)",
+      "D3 < ((D5 - (Period(8, 9, 8) + (Period(7, 6, 2) * 0))) - Period(4, 2, 5)) || D7 != D3",
+      "(B2 != False) -> (Date(2032, 9, 29) == (D2 - (Period(7, 0, 0) + ((Period(1, 4, 7) + Period(4, 6, 8)) * 1)))) || I6 != D6.day",
+      "D9.month != I7 || (B6 == True) -> (D5 != D8)"
     ],
-    "size": 9
+    "size": 6,
+    "execution_time": 53.237571001052856
   }
 """
 
@@ -88,7 +83,7 @@ def main():
     # 3) Execute the generated code with a DateSMTBuilder factory
     def create_builder():
         # You can change approach/implementation if you want
-        return DateSMTBuilder(approach="alpha_beta", implementation="bitvector")
+        return DateSMTBuilder(approach="naive", implementation="int")
 
     exec_globals = {
         "Date": Date,
